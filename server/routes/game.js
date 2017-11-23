19e11097dc3a6bc4b90eaa4c815f323dd2f259ec
kritchang <krit_424@hotmail.com>
2017-11-24 04:58:58 +0700

    add assets

--- server/routes/game.js
+++ server/routes/game.js
@@ -3,20 +3,25 @@ const express = require('express')
 
 const game = express.Router()
 
-game.use(express.static(path.join(__dirname, './public')))
+game.use(express.static(path.join(__dirname, '../../public')))
 game.get('/getPic/:id/:name', (req, res) => {
   if (parseInt(req.params.id, 10) === 1) {
-    res.sendFile(path.resolve(`public/images/${req.params.name}.png`))
+    res.sendFile(path.resolve(`../public/images/${req.params.name}.png`))
   } else res.send('Access denied')
 })
 game.get('/getAnimate/:id/:name/:frame', (req, res) => {
   if (parseInt(req.params.id, 10) === 1) {
-    res.sendFile(path.resolve(`public/images/${req.params.name}_${req.params.frame}.png`))
+    res.sendFile(path.resolve(`../public/images/${req.params.name}_${req.params.frame}.png`))
   } else res.send('Access denied')
 })
-game.get('/getGame/:id/:name/', (req, res) => {
+game.get('/getItem/:id/:name/', (req, res) => {
   if (parseInt(req.params.id, 10) === 1) {
-    res.sendFile(path.resolve(`public/assets/${req.params.name}.png`))
+    res.sendFile(path.resolve(`../public/assets/items/${req.params.name}.png`))
+  } else res.send('Access denied')
+})
+game.get('/getObject/:id/:name/', (req, res) => {
+  if (parseInt(req.params.id, 10) === 1) {
+    res.sendFile(path.resolve(`./public/assets/objects/${req.params.name}.png`))
   } else res.send('Access denied')
 })
 
